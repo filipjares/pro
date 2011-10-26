637166986647bb7923f4484e135172cfe4103075
Filip Jareš <filipjares@gmail.com>
2011-10-26 10:08:51 +0200

    Task 6 solution
    Matrix Z has to be determined yet

---
+++ 05/Assig03.m
@@ -0,0 +1,16 @@
+M7R0
+I*Mechanism<:/%(lambda2G$"?I/mt'[)Hr8c3E!)****!#I/%$mu5G$!?1-&39&)yw:K5`9%****F
+(/%$mu2G$!?'z:5`t\7&*eb'R9$G'!#K/%(lambda6G$"?sVC"yN5LP%QXai)***F(/%(lambda3G$"
+?vx$eghHr'45L%e1\$!#L/%#a4G$"?++++++++++++++**F(/%#a6G$"?++++++++++++++!*!#J/%#
+a5G$"?++++++++++++++EF(/%#d2G$!?++++++++++++++QF(/%#d3G$!?++++++++++++++:F(/%#d
+5G$!?++++++++++++++IFC/%(lambda4G$!?6AF?yd$)3$)*eOG`u"F1/%(lambda5G$!?d*QP()zIe
+m2#=I3#3"FC/%$mu3G$!?)>oQ4q7m;l2R*******F(/%$mu1G$!?F[;btt)GrT1B>'****F(/%$mu6G
+$"?LQ%GNpJGvp(=o)zl"FC/%$mu4G$"?EzC,H!))p(G8pZ)*****F(/%#d4G$"?++++++++++++5MA!
+#F/%#d1G$"?++++++++++++Sr6Fjo/%#a3G$"?++++++++++++!pC#Fjo/%#a2G$"?++++++++++++I
+5@Fjo/%#a1G$"?++++++++++++S(4$Fjo/%#d6G$"?++++++++++++!)G[Fjo/%(lambda1G$"?*3(R
+@))['\$RP)\Nls)F16",
+I$MhV7./%#lxG$"?d5V&[1SN'3a9@RZ\F(/%#lyG$!?])[Y!4I%\EbBPYuE#F(/%#lzG$!?Ie35+8!G
+@+\'zR*Q)F(/%#mxG$!?J6d;MO6gdu)of)[CF(/%#myG$!?ztTi8ba,w\"*y;E'*F(/%#mzG$"?0vXp
+s>c1.*e;uv:"F(/%#nxG$!?+'p$>>k")*y*=m!\#Q$)F(/%#nyG$"?aoyVN;$Gzw)=&[<["F(/%#nzG
+$!?#RS&Q/x<I&HP^3xJ&F(/%#rxG$!?J#zb<rtXf!GU-'R)HFjo/%#ryG$"?)GrJ*e=M<DzLZ2,#*!#
+G/%#rzG$"?P+He")3]"[W(RQR*\$FjoFcq
